d17292814cdb82e2d458dc448da090ff9981d3ee
WilliamWood01 <117728607+WilliamWood01@users.noreply.github.com>
2024-10-18 12:35:30 +0100

    user service

---
+++ src/services/users/users.service.ts
@@ -0,0 +1,72 @@
+import { UserModel } from "./models/user.interface";
+
+
+export class UserService {
+  constructor(private userRepository = new UserRepository()) {}
+
+  public addUser = async (
+    user: Partial<UserModel>,
+    cognitoSub: string
+  ): Promise<UserModel> => {
+    const innerFunctionName = "userService.addUser";
+
+    try {
+      // Validate
+      if (!cognitoSub) throw new Error("CognitoSub missing");
+
+      const createdDate = user.createdDate ?? new Date();
+      const userFull: UserModel = {
+        ...user,
+        familyName: user.familyName as string,
+        email: user.email as string,
+        givenName: user.givenName as string,
+        cognitoId: cognitoSub,
+        id: user.id as string,
+        createdBy: user.email as string,
+        createdDate: createdDate,
+        updatedBy: user.email as string,
+        updatedDate: user.updatedDate ?? createdDate,
+        isAdmin: user.isAdmin ? user.isAdmin : false,
+      };
+
+      try {
+        await this.userRepository.addUser(userFull);
+      } catch (error) {
+        throw error;
+      }
+      return userFull;
+    } catch (e) {
+      const errorMessage = `Failed to add user. ${e}`;
+      console.error({
+        message: errorMessage,
+        innerFunctionName,
+      });
+      throw e;
+    }
+  };
+
+  public getUser = async (
+    userEmail: string
+  ): Promise<UserModel | undefined> => {
+    const innerFunctionName = "userService.getUser";
+    // Validation
+    if (!userEmail) {
+      throw new Error("userEmail");
+    }
+
+    // Fetch end customer
+    let user: UserModel | undefined;
+    try {
+      user = await this.userRepository.getUser(userEmail);
+    } catch (e) {
+      const errorMessage = `Failed to fetch user details. ${e}`;
+      console.error({
+        message: errorMessage,
+        innerFunctionName,
+      });
+      throw e;
+    }
+
+    return user;
+  };
+}
