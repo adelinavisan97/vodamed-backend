5112ee0215ac143ba78e0c05a0a10bd2534351fd
adelinavisan97 <57050387+adelinavisan97@users.noreply.github.com>
2024-11-08 14:03:44 +0000

    Frontend integration changes (#7)
    * fix: cors origin allowed for deployed frontend
    
    * fix:
    
    * fix: :recycle: changed run dev command so it can be run locally after the deployment config was added

--- src/config.ts
+++ src/config.ts
@@ -2,18 +2,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  PORT: 5555,
-  mongoURL: process.env.MONGO_URL!, //Own database URL
-  ClientId: process.env.CLIENT_ID!,
-  Region: process.env.REGION!,
-  ClientSecret: process.env.CLIENT_SECRET!,
-  IdentityPoolId: process.env.IDENTITY_POOL_ID!,
-  UserCollectionName: "user",
-  MedicineCollectionName: "medicine",
-  PrescriptionCollectionName: "prescription",
-  OrderCollectionName: "order",
-  AppointmentCollectionName: "appointment",
-  JwtSecret: process.env.JWT_SECRET!,
-  OpenAPiKey: process.env.OPEN_API_KEY!,
-  DbName: process.env.MONGO_DB_NAME!,
+    PORT: 443,
+    mongoURL: process.env.MONGO_URL!, //Own database URL
+    ClientId: process.env.CLIENT_ID!,
+    Region: process.env.REGION!,
+    ClientSecret: process.env.CLIENT_SECRET!,
+    IdentityPoolId: process.env.IDENTITY_POOL_ID!,
+    UserCollectionName: "user",
+    MedicineCollectionName: "medicine",
+    PrescriptionCollectionName: "prescription",
+    OrderCollectionName: "order",
+    AppointmentCollectionName: "appointment",
+    JwtSecret: process.env.JWT_SECRET!,
+    OpenAPiKey: process.env.OPEN_API_KEY!,
+    DbName: process.env.MONGO_DB_NAME!,
 };
